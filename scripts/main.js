7442580b9ce3f8a7ec291b1455fad073f0dd994d
ak65234 <6294860+ak65234@users.noreply.github.com>
2019-04-11 12:06:46 -0500

    Using map constants; auto-formatted code

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,26 @@ var width = window.innerWidth * 0.5,
     height = window.innerHeight,
     prefix = prefixMatch(["webkit", "ms", "Moz", "O"]);
 
-let mapMinScale = 11;
-let mapMaxScale = 25;
-let mapInitScale = 14;
+// Map scale bounds
+const mapScale = {
+    min: 9,
+    max: 25
+}
+
+// Initial map position
+const mapPosition = {
+    lat: 0,
+    long: 0
+}
+
+/**
+ * Return projection of latitude and logitude
+ * @param {number} latitude 
+ * @param {number} longitude 
+ */
+function latLong(latitude, longitude) {
+    return projection([longitude, latitude]).map(function (x) { return -x; })
+}
 
 d3.csv("data/ted_main.csv", function (error, dataset) { createMap(dataset) });
 
@@ -12,13 +29,13 @@ var tile = d3.geo.tile()
     .size([width, height]);
 
 var projection = d3.geo.mercator()
-    .scale((1 << 25) / 300000)
+    .scale((1 << mapScale.max) / 300000)
     .translate([-width / 2, -height / 2]); // just temporary
 
 var zoom = d3.behavior.zoom()
     .scale(projection.scale() * 2 * Math.PI)
-    .scaleExtent([1 << 9, 1 << 25])
-    .translate(projection([0, 0]).map(function (x) { return -x; }))
+    .scaleExtent([1 << mapScale.min, 1 << mapScale.max])
+    .translate(latLong(mapPosition.lat, mapPosition.long))
     .on("zoom", zoomed);
 
 var container = d3.select("div#map").call(zoom).on("mousemove", mousemoved);
@@ -50,7 +67,7 @@ function createMap(dataset) {
         .enter()
         .append("circle")
         .style("opacity", .5)
-       // .style("opacity", function (d) { return opacityValue(d.year, d.occupation)})
+        // .style("opacity", function (d) { return opacityValue(d.year, d.occupation)})
         .attr("r", function (d) { return radiusScale(d.comments) })
         .attr("cx", function (d) { return projection([d.lon, d.lat])[0] })
         .attr("cy", function (d) { return projection([d.lon, d.lat])[1] })
@@ -120,40 +137,34 @@ function formatLocation(p, k) {
 // var terms = topicise();
 // getWordCloud( terms );
 
-updateWordCloud( [1,2,3] );
+updateWordCloud([1, 2, 3]);
 
-function updateWordCloud( talk_ids )
-{
+function updateWordCloud(talk_ids) {
     var worker_lda = new Worker("scripts/worker/lda_worker.js");
 
-	$.ajax({
-	  type: "GET",  
-	  url: "data/transcripts.csv",
-	  dataType: "text",       
-	  success: function(response)  
-	  {
+    $.ajax({
+        type: "GET",
+        url: "data/transcripts.csv",
+        dataType: "text",
+        success: function (response) {
             var transcripts = [];
             var data = $.csv.toArrays(response);
-            for( var i=0; i<talk_ids.length; i++ )
-            {
+            for (var i = 0; i < talk_ids.length; i++) {
                 transcripts.push(data[talk_ids[i]][1]);
             }
-            worker_lda.postMessage( transcripts );
-            worker_lda.onmessage = function( event )
-            {
-                getWordCloud( event.data );
+            worker_lda.postMessage(transcripts);
+            worker_lda.onmessage = function (event) {
+                getWordCloud(event.data);
             };
 
-	  }   
+        }
     });
 
-    function getWordCloud( terms )
-    {
+    function getWordCloud(terms) {
         var term_count = {};
         var term_topic = {};
 
-        for( var i=0; i<terms.length; i++ )
-        {
+        for (var i = 0; i < terms.length; i++) {
             term_count[terms[i][1]] = terms[i][2];
             term_topic[terms[i][1]] = terms[i][0];
         }
@@ -167,40 +178,39 @@ function updateWordCloud( talk_ids )
         var word_entries = d3.entries(term_count);
 
         var xScale = d3.scale.linear()
-            .domain([0, d3.max(word_entries, function(d) {
+            .domain([0, d3.max(word_entries, function (d) {
                 return d.value;
             })
             ])
-            .range([10,100]);
+            .range([10, 100]);
 
         d3.layout.cloud().size([width, height])
             .timeInterval(20)
             .words(word_entries)
-            .fontSize(function(d) { return xScale(+d.value); })
-            .text(function(d) { return d.key; })
-            .rotate(function() { return ~~(Math.random() * 2) * 90; })
+            .fontSize(function (d) { return xScale(+d.value); })
+            .text(function (d) { return d.key; })
+            .rotate(function () { return ~~(Math.random() * 2) * 90; })
             .font("Impact")
             .on("end", draw)
             .start();
 
-        function draw(words)
-        {
+        function draw(words) {
             d3.select(svg_location).append("svg")
                 .attr("width", width)
                 .attr("height", height)
-            .append("g")
+                .append("g")
                 .attr("transform", "translate(" + [width >> 1, height >> 1] + ")")
-            .selectAll("text")
+                .selectAll("text")
                 .data(words)
-            .enter().append("text")
-                .style("font-size", function(d) { return xScale(d.value) + "px"; })
+                .enter().append("text")
+                .style("font-size", function (d) { return xScale(d.value) + "px"; })
                 .style("font-family", "Impact")
-                .style("fill", function(d, i) {  console.log(d.text); return fill(term_topic[d.text]); })
+                .style("fill", function (d, i) { console.log(d.text); return fill(term_topic[d.text]); })
                 .attr("text-anchor", "middle")
-                .attr("transform", function(d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                .attr("transform", function (d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
                 })
-                .text(function(d) { return d.key; });
+                .text(function (d) { return d.key; });
         }
 
         d3.layout.cloud().stop();
